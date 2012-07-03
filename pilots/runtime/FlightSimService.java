d0c83101dd12eff2c9a30cf576816aa04c370ec6
Shigeru Imai <imais@cs.rpi.edu>
2012-07-03 15:10:15 +0000

    Added FlightSimService
    git-svn-id: https://wolf.cs.rpi.edu/svn/wcl/software/pilots@716 d4fd7ada-4b69-dd11-bbfc-00b0d041e3d9

---
+++ pilots/runtime/FlightSimService.java
@@ -0,0 +1,77 @@
+package pilots.runtime;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Calendar;
+
+import pilots.runtime.*;
+
+
+public class FlightSimService extends DebugPrint implements CurrentLocationTimeService {
+    private Date now_, end_;
+
+    public FlightSimService() {
+        String timeSpan = System.getProperty( "timeSpan" );
+        if (timeSpan == null) {
+            // should throw an exception here
+            System.err.println( "Need \"timeSpan\" property to be defined!!!" );
+            return;
+        }
+
+        String[] timeSpans = timeSpan.split( "~" );
+
+        String datePattern = "yyyy-MM-dd HHmmssZ";
+        DateFormat dateFormat = new SimpleDateFormat( datePattern );
+
+        Date[] time = new Date[2];
+        for (int i = 0; i < 2; i++) {
+            if (timeSpans[i] != null) {
+                time[i] = new Date();
+                try {
+                    time[i] = dateFormat.parse( timeSpans[i] );
+                } catch (ParseException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        now_ = time[0];
+        end_  = time[1];
+    }
+    
+    public Date getTime() {
+        return now_;
+    }
+
+
+    public double[] getLocation() {
+        String[] varNames = { "x", "y", "z" };
+        double[] locations = new double[3];
+        //Method[] methods = { new Method( Method.Closest, "t" ) };
+        Method[] methods = { new Method( Method.Interpolate, "t", "2" ) };
+
+        for (int i = 0; i < 3; i++) {
+            DataStore store = DataStore.findStore( varNames[i] );
+            if (store != null) {
+                locations[i] = store.getData( varNames[i], methods );
+            }
+            else {
+                dbgPrint( "no matching variable stored for \"" + varNames[i] + "\"");
+            }
+        }
+
+        return locations;
+    }
+
+
+    public void progressTime( long offset ) { // offset in msec
+        now_.setTime( now_.getTime() + offset );
+    }
+
+
+    public boolean isEndTime() {
+        return (0 < now_.compareTo( end_ ));
+    }        
+
+}
