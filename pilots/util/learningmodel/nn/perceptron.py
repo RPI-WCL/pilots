2bd6ee8ee28f0fc1164ff5b624705b537208579a
Sida Chen <sida.chen.95+git@gmail.com>
2018-02-19 14:29:12 -0500

    ml: neural network for weight estimation of simulated ATR 72.
    This is the result shown on meeting of Feb 9th with in sample RMSE 200
    and test RMSE 1700. Roll, Pitch, Alpha angle, altitude relative to sea
    level, indicated velocity, and vertical velocity and acceleration are
    used. It's using data from July 12, 13, 15 2016 for training and
    testing.

--- pilots/util/learningmodel/nn/perceptron.py
+++ pilots/util/learningmodel/nn/perceptron.py
@@ -1,101 +1,125 @@
-from __future__ import print_function
-
 import tensorflow as tf
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+import math
+import pandas as pd
+from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import sys
 
-# TODO: random order data feed
+def preprocess(x):
+    scaler = StandardScaler()
+    x = scaler.fit_transform(x)
+    return x, scaler
 
-# Import data
-D = np.genfromtxt('training_data/atr72/training.csv', delimiter=',', skip_header=1)
-num_data = D.shape[0]
-xs = D[:,[0,1,2,4]]
-ys = D[:,3].reshape(num_data,1)
+def split_label(d):
+    return d[:,1:], d[:,0].reshape(d.shape[0], 1)
 
-# Parameters
-learning_rate = 0.01
-training_epochs = 5000
-batch_size = 100
-display_step = 100
+def run(config, layers):
+    learning_rate = config['learning_rate']
+    training_epochs = config['training_epochs']
+    train_data_files = config['train_data_files']
+    test_data_files = config['test_data_files']
 
-# Network Parameters
-n_hidden_1 = 20 # 1st layer number of features
-n_hidden_2 = 10 # 2nd layer number of features
-n_input = 4
-# n_classes = 10 # MNIST total classes (0-9 digits)
+    D = load_data(*train_data_files)
+    x, y = split_label(D)
 
-# tf Graph input
-x = tf.placeholder("float", [None, n_input])
-y_ = tf.placeholder("float", [None, 1])
+    x_train, x_valid, y_train, y_valid = train_test_split(x, y)
+    x_train, scaler = preprocess(x_train)
 
+    # construct neuron network
+    output_layer, feature_placeholder, label_placeholder = make_network(x_train.shape[1], y_train.shape[1], layers)
+    error = tf.losses.mean_squared_error(output_layer, label_placeholder)
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(error)
 
-# Create model
-def multilayer_perceptron(x, weights, biases):
-    # Hidden layer with RELU activation
-    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    layer_1 = tf.nn.relu(layer_1)
-    # Hidden layer with RELU activation
-    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-    layer_2 = tf.nn.relu(layer_2)
-    # Output layer with linear activation
-    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
-    return out_layer
+    # Initializing the variables
+    init = tf.global_variables_initializer()
 
-# Store layers weight & bias
-weights = {
-    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
-    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([n_hidden_2, 1]))
-}
-biases = {
-    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
-    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([1]))
-}
+    # Launch the graph
+    sess = tf.InteractiveSession()
+    sess.run(init)
 
-# Construct model
-y = multilayer_perceptron(x, weights, biases)
+    errors = []
+    for epoch in range(training_epochs):
+        a, e = sess.run([optimizer, error], feed_dict={feature_placeholder: x_train, label_placeholder: y_train})
+        errors.append(e)
+        if epoch % 100 == 0:
+            print "Epoch:", '%04d' % (epoch+1), "error=", "{:.9f}".format(e)
+    plt.plot(errors)
+    plt.yscale('log')
+    plt.show()
 
-# Define loss and optimizer
-# error = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
-error = tf.reduce_sum(tf.square(y_-y))/num_data
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(error)
+    # validation error
+    x_valid = scaler.transform(x_valid)
+    e = sess.run(error, feed_dict={feature_placeholder: x_valid, label_placeholder: y_valid})
+    print 'validation error RMSE = ', e**0.5
+    results = sess.run(output_layer, feed_dict={feature_placeholder: x_valid})
+    plt.plot(np.arange(0,x_valid.shape[0]), results, 'r', y_valid, 'b')
+    plt.show()
 
-# Initializing the variables
-init = tf.global_variables_initializer()
+    # in sample error 
+    D = load_data(*train_data_files)
+    x_insample, y_insample = split_label(D)
+    x_insample = scaler.transform(x_insample)
+    e = sess.run(error, feed_dict={feature_placeholder: x_insample, label_placeholder: y_insample})
+    print 'validation error RMSE = ', e**0.5
+    results = sess.run(output_layer, feed_dict={feature_placeholder: x_insample})
+    plt.plot(np.arange(0,x_insample.shape[0]), results, 'r', y_insample, 'b')
+    plt.show()
 
-# Launch the graph
-sess = tf.InteractiveSession()
-sess.run(init)
+    # testing error
+    D_test = load_data(*test_data_files)
+    x_test, y_test = split_label(D_test)
+    x_test = scaler.transform(x_test)
 
-# Training cycle
-for epoch in range(training_epochs):
-    avg_error = 0.
-    # total_batch = int(mnist.train.num_examples/batch_size)
-    total_batch  = int(num_data/batch_size)
-    # Loop over all batches
-    for i in range(total_batch):
-        # batch_x, batch_y = mnist.train.next_batch(batch_size)
-        batch_x = xs[i*100:(i+1)*100]
-        batch_y = ys[i*100:(i+1)*100].reshape(100,1)
+    results = sess.run(output_layer, feed_dict={feature_placeholder: x_test})
+    e = sess.run(error, feed_dict={feature_placeholder: x_test, label_placeholder: y_test})
+    print 'testing error RMSE = ', e**0.5
+    plt.plot(np.arange(0,x_test.shape[0]), results, 'r', y_test, 'b')
+    plt.show()
+    return output_layer, feature_placeholder, label_placeholder, scaler, error
 
-        # Run optimization op (backprop) and error op (to get loss value)
-        _, e = sess.run([optimizer, error], feed_dict={x: batch_x,
-                                                      y_: batch_y})
-        # Compute average loss
-        avg_error += e / total_batch
-    # Display logs per epoch step
-    if epoch % display_step == 0:
-        print("Epoch:", '%04d' % (epoch+1), "error=", \
-            "{:.9f}".format(avg_error))
+# hyper parameter tuning by grid search
+def make_network(input_size, output_size, hidden_layers_activation_fn):
+    x = tf.placeholder("float", [None, input_size])
+    y_ = tf.placeholder("float", [None, 1])
+    layers = []
+    prev = x
+    prev_num_neuron = input_size
+    for i, layer in enumerate(hidden_layers_activation_fn):
+        num_neuron, activation_func = layer
+        current_layer = tf.add(tf.matmul(prev, tf.Variable(tf.random_normal([prev_num_neuron, num_neuron]))), tf.Variable(tf.random_normal([num_neuron])))
+        if activation_func:
+            current_layer = activation_func(current_layer)
+        layers.append(current_layer)
+        prev = current_layer
+        prev_num_neuron = num_neuron
+    # make the last layer
+    if layers:
+        layers.append(tf.add(tf.matmul(layers[-1], tf.Variable(tf.random_normal([prev_num_neuron, output_size]))), tf.Variable(tf.random_normal([output_size]))))
+    else:
+        layers.append(tf.add(tf.matmul(x, tf.Variable(tf.random_normal([prev_num_neuron, output_size]))), tf.Variable(tf.random_normal([output_size]))))
+    return layers[-1], x, y_
 
-results = sess.run(y,feed_dict={x: xs, y_: ys})
-# print ("------------------------------------------------------------")
-# for i in range(num_data):
-#     print (results[i], ys[i])
+def load_data(*file_names):
+    D = []
+    for file_name in file_names:
+        df = pd.read_csv(file_name)
+        D.append(np.matrix(df))
+    return np.vstack(D)
 
-plt.plot(np.arange(0,num_data), results, 'r', ys, 'b')
-plt.show()
+def main():
+    features = ['_roll__deg', 'pitch__deg', 'alpha__deg', '__altftmsl', '_Vind_kias', 'y_velocity', 'y_acceleration']
+    label = ['curnt___lb']
+    config = {
+        'train_data_files': ['train/20160713_all.csv', 'train/20160715_all.csv'],
+        'test_data_files': ['train/20160712_cruise_phase.csv'],
+        'learning_rate': 0.005, 
+        'training_epochs': 50000,
+        'batch_size': 10000
+    }
+    layers = [(20, tf.nn.sigmoid), (10, tf.nn.relu)]
+    return run(config, layers)
 
-# Test model
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+main()
